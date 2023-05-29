6f6d2062a9aba33433f953be4355c67c48ccff00
jessicaMarquess <jessica.marques.dev@gmail.com>
2023-05-29 20:58:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import styled, { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+import { HeartBreak } from 'phosphor-react'
+
+export const Container = styled.h1`
+  font-family: 'Baloo 2', cursive;
+  font-weight: 800;
+  color: ${(props) => props.theme['purple-dark']};
+`
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container>oi, sou mó legal! confia ✌️</Container>
+      <HeartBreak size={24} color="#4B2995" />
+    </ThemeProvider>
+  )
+}
+
+export default App
