420bb6a37dcfdfe0c9da6ddaa1fa46d9599160f9
jessicaMarquess <jessica.marques.dev@gmail.com>
2023-05-31 20:06:14 -0300

    utilizando do react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
-
-import { HeartBreak } from 'phosphor-react'
-
-export const Container = styled.h1`
-  font-family: 'Baloo 2', cursive;
-  font-weight: 800;
-  color: ${(props) => props.theme['purple-dark']};
-`
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container>oi, sou mó legal! confia ✌️</Container>
-      <HeartBreak size={24} color="#4B2995" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <>
+      <header>Aqui é o header</header>
+      <body> aqui é o body</body>
+    </>
+  )
+}
